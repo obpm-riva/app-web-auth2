e05d798deb0382eff5a15a9e855b69523eef3ca0
Anton Dev <humanistitel@gmail.com>
2018-02-14 11:31:29 +0000

    fix

--- v2/scripts/register-script.js
+++ v2/scripts/register-script.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports={
+module.exports=﻿{
   "en": {
     "translation": {
       "missing-poll": "Missing poll parameter in URL",
@@ -43,7 +43,7 @@ module.exports={
       "permissions-reject": "Reject",
       "permissions-accept": "Accept",
       "login-form-toggle": "Create an account",
-      "login-form-register": "Sign in",
+      "login-form-register": "CREATE",
       "login-username-label": "userID or e-mail",
       "login-password-label": "Password"
     }
