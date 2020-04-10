3b374cc1d3ffb538661c0ef66908397e004d6dcb
Volodymyr Saakian <vsaakian@lohika.com>
2020-04-10 18:26:23 +0300

    Update

--- v2/auth2/service-worker.js
+++ v2/auth2/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.397dad79a3458bb6e210273b3cefa002.js"
+  "/precache-manifest.6325593f204dec6ecf3b48ec8e289f42.js"
 );
 
 self.addEventListener('message', (event) => {
