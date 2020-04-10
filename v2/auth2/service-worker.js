c2ee2616afd57da26a09545c540a5fe30394afcb
Volodymyr Saakian <vsaakian@lohika.com>
2020-04-10 14:39:09 +0300

    Update

--- v2/auth2/service-worker.js
+++ v2/auth2/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8bc787b7c0d4cc13acb5a06c0c70a6c.js"
+  "precache-manifest.3f2b1f38cca6e2443c3bd035a1ff10eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
