c198dc660915645abb93c0d429b798b9d0092da4
Volodymyr Saakian <vsaakian@lohika.com>
2020-04-12 14:58:21 +0300

    Update

--- v2/buildAuth/service-worker.js
+++ v2/buildAuth/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c21527afe6868875818b2f7f1504859.js"
+  "./buildAuth/precache-manifest.357df12de2c494ffb0ff75a6e7f70b38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./buildAuth/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
